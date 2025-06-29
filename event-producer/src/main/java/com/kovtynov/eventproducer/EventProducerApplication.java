a148edaa26fb5c19111cf6367cb8461c621e6325
WoldemarK <kovtynov.vladimir@gmail.com>
2025-06-29 20:50:19 +0300

    added event-producer and basic configs

---
+++ event-producer/src/main/java/com/kovtynov/eventproducer/EventProducerApplication.java
@@ -0,0 +1,43 @@
+package com.kovtynov.eventproducer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@SpringBootApplication
+public class EventProducerApplication implements CommandLineRunner {
+
+   // private final KafkaTemplate<String, Event> kafkaTemplate;
+
+//    @Value("${event.topic}")
+//    private String topic;
+//
+//    public EventProducerApplication(KafkaTemplate<String, Event> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args)  {
+        System.out.println("Avro Event producer started");
+    }
+
+    @Scheduled(fixedRateString = "${event.generation.interval-ms}")
+    public void sendEvent() {
+//        Event event = Event.newBuilder()
+//                .setUid(UUID.randomUUID().toString())
+//                .setSubject("subject")
+//                .setDescription("description")
+//                .build();
+//
+//        kafkaTemplate.send(new ProducerRecord<>(topic, event.getUid(), event));
+//        System.out.println("Sent: " + event);
+    }
+}
