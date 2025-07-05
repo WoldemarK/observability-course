5a5095dea30dc460713ef6d3bc773fd1ea89b7a9
WoldemarK <kovtynov.vladimir@gmail.com>
2025-07-05 18:37:19 +0300

    Починил сборку

--- event-consumer/src/main/java/com/kovtynov/eventconsumer/model/EventEntity.java
+++ event-consumer/src/main/java/com/kovtynov/eventconsumer/model/EventEntity.java
@@ -1,6 +1,5 @@
 package com.kovtynov.eventconsumer.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
