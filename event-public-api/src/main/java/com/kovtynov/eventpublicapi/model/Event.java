9f3dbc831a2593a4eb03e3bc00cef75f00575700
WoldemarK <kovtynov.vladimir@gmail.com>
2025-07-07 00:19:12 +0300

    Сборка,отрабатывает

--- event-public-api/src/main/java/com/kovtynov/eventpublicapi/model/Event.java
+++ event-public-api/src/main/java/com/kovtynov/eventpublicapi/model/Event.java
@@ -1,4 +1,10 @@
 package com.kovtynov.eventpublicapi.model;
 
-public record Event(String uid, String subject, String description, String traceId) {
+public record Event
+        (
+                String uid,
+                String subject,
+                String description,
+                String traceId
+        ) {
 }
